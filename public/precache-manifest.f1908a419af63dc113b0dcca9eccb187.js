bc9689f11797228b579d35723e8f5f0c89a835e2
Gullies <1448991007@qq.com>
2020-07-26 01:07:06 +0800

    Generate merge project template

---
+++ public/precache-manifest.f1908a419af63dc113b0dcca9eccb187.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "055e0c12295c5f40034382025f9671fb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cc7706406719470ec60d",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7369b7bf7c42fc397f3f",
+    "url": "/static/js/2.27824895.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.27824895.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc7706406719470ec60d",
+    "url": "/static/js/main.007158e7.chunk.js"
+  },
+  {
+    "revision": "c21438550d3812e8d482",
+    "url": "/static/js/runtime-main.32448414.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
