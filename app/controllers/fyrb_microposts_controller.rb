b2c1b381da495cf823efc84e8c40207d11f2b1b5
Gullies <1448991007@qq.com>
2020-12-12 02:09:32 +0800

    temp1212_6

--- app/controllers/fyrb_microposts_controller.rb
+++ app/controllers/fyrb_microposts_controller.rb
@@ -6,8 +6,14 @@ class FyrbMicropostsController < ApplicationController
   
   def index
     # @microposts = FyrbMicropost.all
-    @microposts = FyrbMicropost.paginate(page: params[:page]||1, per_page: 5)
-    render json: @microposts, status: :ok
+    @microposts = FyrbMicropost.paginate(page: params[:page]||1, per_page: 6)
+    # render json: @microposts, status: :ok
+    render json: {
+      :current_page => @microposts.current_page,
+      :per_page => @microposts.per_page,
+      :total_entries => @microposts.total_entries,
+      :entries => @microposts
+    }, status: :ok
   end
 
   def show
